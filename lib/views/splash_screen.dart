45795b6702ac843e07186b5cc54ac6fcab101d87
kamran saleem <freinds221@gmail.com>
2022-10-23 01:20:23 +0500

    in Progress

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:blood_donor/views/onboardin_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        const Duration(seconds: 3),
+        (() => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => const OnBoardingScreen())))));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+            child: Text(
+      'W E L C O M E',
+      style: TextStyle(fontSize: 30),
+    )));
+  }
+}
